8f98d0db30fe11c82ad0fa5a04363006b7f94b04
Hamza Maach <hamzamaach56@gmail.com>
2023-12-06 15:04:22 +0100

    affichage des announces et avenement dans l'accueil

--- src/components/accueil/main/main.jsx
+++ src/components/accueil/main/main.jsx
@@ -1,460 +1,191 @@
 import React, { useEffect, useState } from "react";
-import {
-  getPublicPosts,
-  PosterPost,
-  SupprimerPost,
-  UpdatePost,
-} from "../../../app/api/postAxios";
-import { selectCurrentUser } from "../../../features/auth/authSlice";
-import LoadingSpinner from "../../loadingSpinner/LoadingSpinner";
 import { useDispatch, useSelector } from "react-redux";
-import GetCookie from "../../../cookies/JWT/GetCookie";
-import UpdatePostAlert from "./post/UpdatePostAlert";
-import SearchIcon from "@mui/icons-material/Search";
 import { toast } from "react-toastify";
-import AddPost from "./post/AddPost";
 import { debounce } from "lodash";
-import Post from "./post/Post";
-import "./main.css";
+import SearchIcon from "@mui/icons-material/Search";
 import {
   Button,
-  ButtonGroup,
   CircularProgress,
   InputAdornment,
   TextField,
 } from "@mui/material";
+import Announce from "./announce/Announce";
+import LoadingSpinner from "../../loadingSpinner/LoadingSpinner";
+import {
+  getAccueilAnnounces,
+  searchAccueilAnnounces,
+} from "../../../app/api/announceAxios";
+import { selectCurrentUser } from "../../../features/auth/authSlice";
+import GetCookie from "../../../cookies/JWT/GetCookie";
+import "./main.css";
+import Evenement from "./Evenement";
 
 const Main = () => {
-  const [posts, setPosts] = useState([]);
-  const [filieres, setFilieres] = useState([]);
+  const [announces, setAnnounces] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isLoadingMore, setIsLoadingMore] = useState(false);
   const [page, setPage] = useState(1);
-  const [err, setErr] = useState();
-  const curUser = useSelector(selectCurrentUser);
-  const token = GetCookie("jwt");
-  const [activeButton, setActiveButton] = useState("all");
-  const dispatch = useDispatch();
+  const [searchPage, setSearchPage] = useState(1);
+  const [lastPage, setLastPage] = useState(null);
+  const [curPage, setCurPage] = useState(null);
   const [searchValue, setSearchValue] = useState("");
-  const [showUpdateAlert, setShowUpdateAlert] = useState(false);
-  const [selectedPost, setSelectedPost] = useState(null);
-  const [refetch, setRefetch] = useState(false);
+  const curUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
 
-  const fetchPosts = async (get = null) => {
+  const fetchAnnounces = async (currentPage, loading = null) => {
     try {
-      getPublicPosts(activeButton, searchValue, token)
-        .then((data) => {
-          if (data) {
-            if (get) {
-              setPosts((prevPosts) => [...prevPosts, ...data?.postes]);
-            } else {
-              setPosts(data?.postes);
-            }
-            setFilieres(data.filieres);
-          }
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setErr(error);
-        });
-    } catch (error) {
-      setErr(error);
-      console.log(err);
-      setIsLoading(false);
-    }
-  };
-
-  const fetchMorePosts = async () => {
-    setIsLoadingMore(true);
-    try {
-      getPublicPosts(activeButton, null, token, page + 1)
-        .then((data) => {
-          if (data) {
-            setPosts((prevPosts) => [...prevPosts, ...data.postes]);
-            setPage(page + 1);
-          }
-          setIsLoadingMore(false);
-        })
-        .catch((error) => {
-          setErr(error);
-        });
-    } catch (error) {
-      setErr(error);
-      console.log(err);
-    }
-  };
-
-  const handlePosterPost = (post) => {
-    PosterPost(post, token)
-      .then((data) => {
-        console.log(data.message);
-        console.log(data.image_data);
-        if (data.message === "success") {
-          post = {
-            ...post,
-            id: data.post_id,
-            user_id: curUser.id,
-            nom: curUser.nom,
-            prenom: curUser.prenom,
-            role: curUser.role,
-            reacts: 0,
-            pdf_path: data.pdf_path,
-            created_at: new Date().toLocaleString("en-US", {
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-              hour: "2-digit",
-              minute: "2-digit",
-            }),
-          };
-          setRefetch(!refetch);
-          setPosts((prevPosts) => [post, ...prevPosts]);
-
-          toast.success("Publié avec succès", {
-            position: "top-center",
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
-        }
-      })
-      .catch((error) => {
-        setErr(error);
-        console.log(err);
-        toast.error(err, {
-          position: "top-center",
-          autoClose: 4000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      });
-  };
-
-  const handleUpdatePost = (updatedPost) => {
-    UpdatePost(updatedPost, token)
-      .then((data) => {
-        console.log(data.message);
-
-        if (data.message === "success") {
-          setPosts((prevPosts) =>
-            prevPosts.map((post) =>
-              post.id === updatedPost.id ? updatedPost : post
-            )
-          );
-          toast.success("Modifié avec succès", {
-            position: "top-center",
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
-          setRefetch(!refetch);
-        }
-      })
-      .catch((error) => {
-        setErr(error);
-        console.log(err);
-        toast.error(err, {
-          position: "top-center",
-          autoClose: 4000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      });
-  };
-
-  const handleDeletePost = (deletedPost) => {
-    if (deletedPost.user_id === curUser.id || curUser.role === "admin") {
-      SupprimerPost(deletedPost.id, token)
-        .then((data) => {
-          console.log(data.message);
-          if (data.message === "success") {
-            setPosts((prevPosts) =>
-              prevPosts.filter((p) => p.id !== deletedPost.id)
-            );
-            toast.success("Supprimé avec succès", {
-              position: "top-center",
-              autoClose: 4000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "light",
-            });
-            setRefetch(!refetch);
-          }
-        })
-        .catch((error) => {
-          setErr(error);
-          console.log(err);
-          toast.error(err, {
-            position: "top-center",
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
-        });
-    } else {
-      console.log("Vous n'avez pas le droit d'effacer ce poste");
-    }
-  };
-
-  const handleUpdateCallback2 = (post) => {
-    setSelectedPost(post);
-    setShowUpdateAlert(true);
-  };
-
-  const handleClose = () => {
-    setShowUpdateAlert(false);
-  };
-
-  const handleSearch = (e) => {
-    setSearchValue(e.target.value);
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetchPosts(false);
-  }, [activeButton, refetch]);
-
-  useEffect(() => {
-    const fetchData = async (loading = null) => {
-      try {
-        if (loading) {
-          setIsLoading(true);
-        }
-        if (curUser) {
-          const data = await getPublicPosts(activeButton, searchValue, token);
-          setPosts(data.postes);
-          setIsLoading(false);
-        } else {
-          const data = await getPublicPosts(activeButton, searchValue);
-          setPosts(data.postes);
-          setIsLoading(false);
-        }
-      } catch (error) {
-        console.log(error);
+      if (loading) {
+        setIsLoading(true);
       }
-    };
+      getAccueilAnnounces(currentPage)
+        .then((data) => {
+          const newDataArray = Object.values(data?.data);
+          setAnnounces((prevAnnounces) => [...prevAnnounces, ...newDataArray]);
+          setCurPage(data.current_page);
+          setLastPage(data.last_page);
+          // setPage(data?.current_page);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.error(error);
+      toast.error("Error fetching announcements");
+    }
+  };
 
-    const delayedFetchData = debounce(fetchData, 1500);
+  const searchAnnounces = async (q, currentPage, loading = null) => {
+    try {
+      if (loading) {
+        setIsLoading(true);
+      }
+      searchAccueilAnnounces(q, currentPage)
+        .then((data) => {
+          if (loading) {
+            setAnnounces(data.data);
+            setCurPage(data.current_page);
+            setLastPage(data.last_page);
+          } else {
+            const newDataArray = Array.isArray(data.data)
+              ? data.data
+              : Object.values(data?.data);
+            setAnnounces((prevAnnounces) => [
+              ...prevAnnounces,
+              ...newDataArray,
+            ]);
+          }
 
-    if (curUser) {
-      delayedFetchData(true);
-    } else {
-      delayedFetchData(true);
+          setCurPage(data.current_page);
+          setLastPage(data.last_page);
+          setIsLoading(false);
+          // setPage(data?.current_page);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.error(error);
+      toast.error("Error fetching announcements");
+    }
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => {
+      const nextPage = prevPage + 1;
+      fetchAnnounces(nextPage, false);
+      return nextPage;
+    });
+  };
+
+  useEffect(() => {
+    let timerId;
+
+    if (searchValue !== "") {
+      // Clear previous timer
+      clearTimeout(timerId);
+
+      // Set a new timer to execute after 2 seconds
+      timerId = setTimeout(() => {
+        searchAnnounces(searchValue, searchPage, true);
+      }, 2000);
     }
 
+    // Clean up the timer on component unmount or when searchValue changes
     return () => {
-      delayedFetchData.cancel();
+      clearTimeout(timerId);
     };
-  }, [curUser, searchValue]);
+  }, [searchValue, searchPage]);
 
-  if (posts) {
-    return (
-      <div id="container-main">
-        <div className="main-post-header">
-          <div className="main-post-filter">
-            <ButtonGroup
-              disableElevation
-              sx={{
-                borderRadius: "10px",
-              }}
-              aria-label="Disabled elevation buttons"
-            >
-              <Button
-                size="small"
-                sx={{
-                  borderRadius: "20px 0 0 20px",
-                  borderColor: "#e86928",
-                  color: activeButton === "all" ? "white" : "#e86928",
-                  "&:hover": {
-                    borderColor: "#e86928",
-                    bgcolor: "#fff8f2",
-                  },
-                  bgcolor: activeButton === "all" ? "#e86928" : "white",
-                  ...(activeButton === "all" && {
-                    "&:hover": {
-                      bgcolor: "#d46025",
-                    },
-                  }),
-                }}
-                variant={activeButton === "all" ? "contained" : "outlined"}
-                onClick={() => setActiveButton("all")}
-              >
-                Tous
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  borderRadius: "20px 0 0 20px",
-                  borderColor: "#e86928",
-                  color: activeButton === "announce" ? "white" : "#e86928",
-                  bgcolor: activeButton === "announce" ? "#e86928" : "white",
-                  "&:hover": {
-                    borderColor: "#e86928",
-                    bgcolor: "#fff8f2",
-                  },
-                  ...(activeButton === "announce" && {
-                    "&:hover": {
-                      bgcolor: "#d46025",
-                    },
-                  }),
-                }}
-                variant={activeButton === "announce" ? "contained" : "outlined"}
-                onClick={() => setActiveButton("announce")}
-              >
-                Announces
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  borderRadius: "20px 0 0 20px",
-                  borderColor: "#e86928",
-                  color: activeButton === "cour" ? "white" : "#e86928",
-                  bgcolor: activeButton === "cour" ? "#e86928" : "white",
-                  "&:hover": {
-                    borderColor: "#e86928",
-                    bgcolor: "#fff8f2",
-                  },
-                  ...(activeButton === "cour" && {
-                    "&:hover": {
-                      bgcolor: "#d46025",
-                    },
-                  }),
-                }}
-                variant={activeButton === "cour" ? "contained" : "outlined"}
-                onClick={() => setActiveButton("cour")}
-              >
-                Professionnel
-              </Button>
-              <Button
-                size="small"
-                sx={{
-                  borderRadius: "20px 0 0 20px",
-                  borderColor: "#e86928",
-                  color: activeButton === "exercice" ? "white" : "#e86928",
-                  bgcolor: activeButton === "exercice" ? "#e86928" : "white",
-                  "&:hover": {
-                    borderColor: "#e86928",
-                    bgcolor: "#fff8f2",
-                  },
-                  ...(activeButton === "exercice" && {
-                    "&:hover": {
-                      bgcolor: "#d46025",
-                    },
-                  }),
-                }}
-                variant={activeButton === "exercice" ? "contained" : "outlined"}
-                onClick={() => setActiveButton("exercice")}
-                style={{ borderRadius: " 0 20px  20px 0" }}
-              >
-                événements
-              </Button>
-            </ButtonGroup>
-          </div>
-          <TextField
-            className="main-post-search"
-            value={searchValue}
-            onChange={handleSearch}
-            size="small"
-            id="outlined-search"
-            label="Chercher publications..."
-            type="search"
-            InputProps={{
-              style: { borderRadius: "20px" },
-              endAdornment: (
-                <InputAdornment position="end">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-            sx={{
-              color: "#1DA1F2",
-            }}
-          />
-        </div>
-        {curUser &&
-          (curUser.role === "admin" || curUser.role === "formateur") && (
-            <AddPost
-              onSubmit={handlePosterPost}
-              filieres={filieres}
-              user={curUser}
-            />
-          )}
-        {isLoading ? (
-          <LoadingSpinner />
-        ) : (
-          <>
-            {posts.map((post, index) => (
-              <Post
-                key={index}
-                post={post}
-                index={index}
-                onSubmit={handleDeletePost}
-                setPosts={setPosts}
-                handleUpdateCallback2={() => handleUpdateCallback2(post)}
-              />
-            ))}
-            {searchValue.length === 0 && (
-              <div className="div_get_more">
-                {isLoadingMore ? (
-                  <CircularProgress />
-                ) : posts.length !== 0 ? (
-                  <Button
-                    onClick={fetchMorePosts}
-                    variant="outlined"
-                    style={{
-                      borderRadius: "20px",
-                      marginTop: "0.5rem",
-                    }}
-                  >
-                    Avoir plus
-                  </Button>
-                ) : (
-                  <span className="main-no-posts">
-                    Aucune publication trouvée !
-                  </span>
-                )}
-              </div>
-            )}
-          </>
-        )}
-
-        {showUpdateAlert && (
-          <UpdatePostAlert
-            onSubmit={handleUpdatePost}
-            post={selectedPost}
-            open={true}
-            filieres={filieres}
-            handleClose={handleClose}
-          />
-        )}
+  useEffect(() => {
+    if (searchValue === "") {
+      fetchAnnounces(page, true);
+    }
+  }, [searchValue]);
+  return (
+    <div id="container-main">
+      <div className="main-post-header">
+        <TextField
+          className="main-post-search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          size="small"
+          id="outlined-search"
+          label="Chercher publications..."
+          type="search"
+          InputProps={{
+            style: { borderRadius: "20px" },
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            color: "#1DA1F2",
+          }}
+        />
       </div>
-    );
-  } else {
-    return null;
-  }
+
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          {announces.map((item, index) =>
+            item.type === "announce" ? (
+              <Announce key={index} announce={item} searchValue={searchValue} />
+            ) : (
+              <Evenement key={index} event={item} searchValue={searchValue} />
+            )
+          )}
+
+          <div className="div_get_more">
+            {announces.length !== 0 ? (
+              curPage !== lastPage ? (
+                <Button
+                  variant="outlined"
+                  style={{
+                    borderRadius: "20px",
+                    marginTop: "0.5rem",
+                  }}
+                  onClick={handleLoadMore}
+                >
+                  Avoir plus
+                </Button>
+              ) : (
+                <span className="main-no-posts">
+                  Pas d'autres annonces disponibles.
+                </span>
+              )
+            ) : (
+              <span className="main-no-posts">
+                Aucune publication trouvée !
+              </span>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Main;
--- src/components/accueil/right/right.jsx
+++ src/components/accueil/right/right.jsx
@@ -12,9 +12,58 @@ import { getRandomFourAdherents } from "../../../app/api/adherentAxios";
 
 const Right = () => {
   const [notifs, setNotifs] = useState([]);
-  const [fourAdherents, setFourAdherents] = useState([]);
+  const [fourAdherents, setFourAdherents] = useState([
+    {
+      id: 9,
+      user_id: 46,
+      secteur_id: 3,
+      propos:
+        "Itaque ut earum ea omnis et ut. Sed delectus expedita voluptatem vitae eligendi.",
+      profession: "Enim unde facere.",
+      ville: "Rabat",
+      created_at: "2023-10-25T11:38:05.000000Z",
+      updated_at: "2023-10-25T11:38:05.000000Z",
+      img_path: null,
+    },
+    {
+      id: 3,
+      user_id: 82,
+      secteur_id: 1,
+      propos:
+        "Voluptas amet iste sit harum molestiae. Non itaque ipsam molestiae consequatur voluptatem.",
+      profession: "Ea dolorem magnam.",
+      ville: "Nador",
+      created_at: "2023-10-25T11:38:04.000000Z",
+      updated_at: "2023-11-02T11:53:57.000000Z",
+      img_path: "adherents/Ps2KzYq2YaVAzkZN97VSga8kh0cWFbcxAxeFzCwT.jpg",
+    },
+    {
+      id: 4,
+      user_id: 34,
+      secteur_id: 2,
+      propos:
+        "Cumque ea aperiam eius est nemo et vero. Pariatur velit aut est. Amet quia quae enim.",
+      profession: "Dolores fugit sequi.",
+      ville: "Oujda",
+      created_at: "2023-10-25T11:38:04.000000Z",
+      updated_at: "2023-11-02T11:54:44.000000Z",
+      img_path: "adherents/tJCMqXiTu7i18rTSrRzWIOfE6PrATWRqujMRoRfQ.jpg",
+    },
+    {
+      id: 1,
+      user_id: 6,
+      secteur_id: 2,
+      propos:
+        "aaHamzaaaVolupsssssssssssssssssssssssstatem iure aliquam occaecati. Doloribus facere quia minus quod deserunt.fffffffffffffffffffffffffffff--6666fffffffffffffffffffffffff",
+      profession: "a",
+      ville: "Berkane",
+      created_at: "2023-10-25T11:38:03.000000Z",
+      updated_at: "2023-11-10T16:23:08.000000Z",
+      img_path: null,
+    },
+  ]);
   const [isLoadingNotif, setIsLoadingNotif] = useState(true);
-  const [isLoadingStag, setIsLoadingStag] = useState(true);
+  const [isLoadingStag, setIsLoadingStag] = useState(false);
   const [errNotif, setErrNotif] = useState();
   const [errStag, setErrStag] = useState();
 
@@ -31,34 +80,33 @@ const Right = () => {
     }
   };
 
-  const fetchRandomFourAdherents = async () => {
-    try {
-      getRandomFourAdherents().then((data)=>{
-        setFourAdherents(data)
-        setIsLoadingStag(false);
-      })
-    } catch (error) {
-      console.log(error);
-      setErrStag(error);
-      setIsLoadingStag(false);
-    }
-  };
-
+  // const fetchRandomFourAdherents = async () => {
+  //   try {
+  //     getRandomFourAdherents().then((data) => {
+  //       setFourAdherents(data);
+  //       setIsLoadingStag(false);
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     setErrStag(error);
+  //     setIsLoadingStag(false);
+  //   }
+  // };
 
   const events = [
-    { id: 1, date: '2023-06-15', color: 'red' },
-    { id: 2, date: '2023-06-20', color: 'blue' },
-    { id: 3, date: '2023-06-25', color: 'green' },
+    { id: 1, date: "2023-06-15", color: "red" },
+    { id: 2, date: "2023-06-20", color: "blue" },
+    { id: 3, date: "2023-06-25", color: "green" },
     // ... add more events here
   ];
-  useEffect(() => {
-    // fetchNotifs();
-    fetchRandomFourAdherents();
-    const interval = setInterval(() => {
-      // fetchNotifs();
-    }, 5 * 60 * 1000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   // fetchNotifs();
+  //   fetchRandomFourAdherents();
+  //   const interval = setInterval(() => {
+  //     // fetchNotifs();
+  //   }, 5 * 60 * 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <div id="container-right">
@@ -86,8 +134,8 @@ const Right = () => {
               )
             )
           } */}
-          <div className="miniCalandar" >
-          <MiniCalendar events={events}/>
+          <div className="miniCalandar">
+            <MiniCalendar events={events} />
           </div>
           <div to="/stagiaires" id="show-more-box">
             <Link id="show-more-btn">Voir plus...</Link>
